2efd688f80a4645ec600b9b86855509998a81bb0
ffagner <farias.live@gmail.com>
2021-03-23 23:44:04 -0300

    criada funçao de de captura dos dados

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,25 @@
-let weight = document.querySelector('.weight')
-console.log(weight)
+function calImc (){
+let form = document.querySelector('.dados');
+let resul = document.querySelector('.resultado');
+let tabela = document.querySelector('.tabela');
+
+function capData(evento){
+    evento.preventDefault()
+
+    let weight = document.querySelector('.weight');
+    let height = document.querySelector('.height');
+    weight = parseFloat(weight.value);
+    height = parseFloat(height.value);
+    // formula de calculo do IMC: peso/altura*altura
+    const cal = (w, h) => {return w/h**2}
+    resul.innerHTML = `<div>${cal(weight, height)}</div>`
+    
+
+}
+
+
+
+form.addEventListener('submit', capData)
+}
+calImc();
+
