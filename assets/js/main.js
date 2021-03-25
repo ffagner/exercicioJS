31fe45fdb83ba8ee80cedc2bda8474600cd506b4
ffagner <farias.live@gmail.com>
2021-03-24 22:48:06 -0300

    projeto finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,55 @@
-function calImc (){
-let form = document.querySelector('.dados');
-let resul = document.querySelector('.resultado');
-let tabela = document.querySelector('.tabela');
+function calImc() {
 
-function capData(evento){
-    evento.preventDefault()
+  let form = document.querySelector(".dados");
+  let resul = document.querySelector(".resultado");
+  let tabela = document.querySelector(".tabela");
 
-    let weight = document.querySelector('.weight');
-    let height = document.querySelector('.height');
+  function capData(evento) {
+    evento.preventDefault();
+    window.location.reload()
+
+    let weight = document.querySelector(".weight");
+    let height = document.querySelector(".height");
     weight = parseFloat(weight.value);
     height = parseFloat(height.value);
     // formula de calculo do IMC: peso/altura*altura
-    const cal = (w, h) => {return w/h**2}
-    resul.innerHTML = `<div>${cal(weight, height)}</div>`
+    let cal = (w, h) => {
+      return w / h ** 2;
+    };
+    cal = cal(weight, height).toFixed(2);
+    let valor = parseFloat(cal);
     
+    function determinar (val) {
+        if(val < 18.50){
+        const row1 = document.getElementById('nivel1')
+        row1.style.backgroundColor = "rgba(17, 86, 102, .3)";
 
+    }   else if (valor >= 18.50 && valor <= 24.90) {
+        const row1 = document.getElementById('nivel2')       
+        row1.style.backgroundColor = "rgba(17, 86, 102, .3)";
+
+    }   else if (valor >= 25.00 && valor <= 29.90) {
+        const row1 = document.getElementById('nivel3')
+        row1.style.backgroundColor = "rgba(17, 86, 102, .3)";
+
+    }   else if (valor >= 30.00 && valor <= 34.90) {
+        const row1 = document.getElementById('nivel4')
+        row1.style.backgroundColor = "rgba(17, 86, 102, .3)";
+
+    }   else if (valor >= 35.00 && valor <= 40.90) {
+        const row1 = document.getElementById('nivel5')
+        row1.style.backgroundColor = "rgba(17, 86, 102, .3)";
+
+    }   else if (valor >= 40.00) {
+        const row1 = document.getElementById('nivel6')
+        row1.style.backgroundColor = "rgba(17, 86, 102, .3)";      
+    }
 }
 
+    determinar(valor)
+    resul.innerHTML = `<div>${cal}</div>`;
+  }
 
-
-form.addEventListener('submit', capData)
+  form.addEventListener("submit", capData);
 }
 calImc();
-
