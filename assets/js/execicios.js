0ba11299a6e086065227681382960fb8a8842a60
ffagner <farias.live@gmail.com>
2021-04-05 12:32:59 -0300

    exercicios

---
+++ assets/js/execicios.js
@@ -0,0 +1,16 @@
+//Exercício 01
+// escreva uma função que receba dois números e retorne o maior deles;
+
+//Exercício 02
+//Escreva uma função chamada ePaisagem que receba dois argumentos largura e altura de uma imagem e devolva verdadeiro ou falso caso ela esta no modo paisagem, ou seja, se: 
+//altura > largura = false
+//altura < largura = true
+
+//Exercício 03
+//escreva uma função que retorne o segunte:
+//numero é divisível por 3 = Fizz 
+//numero é divisível por 5 = Buzz 
+//numero é divisível por 3 e 5 = FizzBuzz 
+//numero NÃO É divisível por 3 e 5 = retornar o proprio numero
+//checar se o caracter é realmente um numero
+// usar rage de 0 a 100 
