c9a2ec5cd1da91499eb11ad406e181ad093764c0
ffagner <farias.live@gmail.com>
2021-04-07 11:00:41 -0300

    setInterval e setTimeout

---
+++ assets/js/setInterval.js
@@ -0,0 +1,18 @@
+//setInterval e setTimeout
+// função retorna a hora atual no formato br 00:00:00
+function hours(){
+    let data = new Date()
+    return data.toLocaleTimeString('pt-br', {hour12:false}) // formatar a string para o formato de hora brasileiro
+}
+
+// setInterval(() => console.log(hours()), 1000) // a função cria um time de execução a cada segundo
+
+const timer = setInterval(() => console.log(hours()), 1000) // retorna à variavel um time de 1 segundo
+
+setTimeout(() => {
+    clearInterval(timer)
+}, 5000);
+
+setTimeout(() => {
+    console.log('fim do tempo')
+}, 5000);
