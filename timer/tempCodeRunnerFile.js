71a1e208a4c3c3f5a251757bde5db35c79c6c0d4
ffagner <farias.live@gmail.com>
2021-04-07 21:16:49 -0300

    iniciado o projeto de um timer.

---
+++ timer/tempCodeRunnerFile.js
@@ -0,0 +1,26 @@
+if (a === 1){
+//     console.log('segunda')
+// }
+// if (a === 2){
+//     console.log('terça')
+
+//     if (a === 3){
+//         console.log('quarta')
+
+//         if (a === 4){
+//             console.log('quita')
+
+//             if (a === 5){
+//                 console.log('sexta')
+
+//                 if (a === 6){
+//                     console.log('sabado')
+
+//                     if (a === 7){
+//                         console.log('domingo')
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
